478f7f122cda522e44b9a12afa4fd3a5b8052c09
Valentin Thevoz <valentin.thevoz@gmail.com>
2025-02-24 17:57:48 -0600

    got bot working

--- snitch_bot.py
+++ snitch_bot.py
@@ -3,9 +3,17 @@ import asyncio
 import image_downloader as imd
 import guild_graveyard as gg
 import json
+import os
+from dotenv import load_dotenv
+from pathlib import Path
+
+load_dotenv(Path('/Users/valentinthevoz/Desktop/Python projects/RealmDeathSnitch/.venv/include/keys.env'))
+
 
 intents = discord.Intents.default()
 client = discord.Client(intents=intents)
+channel_id = int(os.getenv("CHANNEL_ID"))
+channel = None
 
 imd.Download_Images()
 with open('last death.json', 'w') as f:
@@ -14,7 +22,16 @@ f.close()
 
 async def run_guild_graveyard():
     while True:
-        gg.guild_graveyard("Anti Furry Force", 0)
+        print("update")
+        latest_death = gg.guild_graveyard("Anti Furry Force", 0)
+        if latest_death != json.load(open('last death.json')) and latest_death['player-name'] != "private":
+            print("someone died")
+            with open('last death.json', 'w') as f:
+                json.dump(latest_death, f)
+            f.close()
+            channel = client.get_channel(int(channel_id))
+            await channel.send(f"**{latest_death['player-name']}** died at **{latest_death['time']}**\n"
+                               f"**Killer:** {latest_death['killed_by']}\n")
         await asyncio.sleep(60)  # Wait for 60 seconds
 
 @client.event
@@ -22,4 +39,5 @@ async def on_ready():
     print(f'We have logged in as {client.user}')
     client.loop.create_task(run_guild_graveyard())
 
-client.run('YOUR_BOT_TOKEN')
+discord_key = os.getenv("DISCORD_KEY")
+client.run(discord_key)
